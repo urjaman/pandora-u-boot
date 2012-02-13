4a0afbce7d3d60f4ad8b2e1f1f5658e0a33761cd
Grazvydas Ignotas <notasas@gmail.com>
2012-02-13 17:59:46 +0200

    update default boot commands
    - read autoboot.txt from NAND too (boot partition)
    - stop sending system console to serial that almost nobody has
    - make ssource command more likely to work (if you pass size)
    
    Author:    Grazvydas Ignotas <notasas@gmail.com>
    Conflict resolution by Urja Rannikko <urjaman@gmail.com>
    
    Signed-off-by: Urja Rannikko <urjaman@gmail.com>

--- include/configs/omap3_pandora.h
+++ include/configs/omap3_pandora.h
@@ -182,15 +182,18 @@
 	"usbtty=cdc_acm\0" \
 	"loadaddr=0x82000000\0" \
 	"bootargs=ubi.mtd=4 ubi.mtd=3 root=ubi0:rootfs rootfstype=ubifs " \
-		"rw rootflags=bulk_read vram=6272K omapfb.vram=0:3000K\0" \
+	"rw rootflags=bulk_read vram=6272K omapfb.vram=0:3000K quiet\0" \
 	"mtdparts=" MTDPARTS_DEFAULT "\0" \
 
 #define CONFIG_BOOTCOMMAND \
 	"if mmc rescan && fatload mmc1 0 ${loadaddr} autoboot.txt || " \
 			"ext2load mmc1 0 ${loadaddr} autoboot.txt; then " \
-		"ssource ${loadaddr}; " \
+		"ssource ${loadaddr} ${filesize}; " \
 	"fi; " \
-	"ubi part boot && ubifsmount boot && ubifsload ${loadaddr} uImage && bootm ${loadaddr}; " \
+	"if ubi part boot && ubifsmount boot; then " \
+		"ubifsload ${loadaddr} autoboot.txt && ssource ${loadaddr} ${filesize}; " \
+		"ubifsload ${loadaddr} uImage && bootm ${loadaddr}; " \
+	"fi; " \
 	"setenv stdout lcd; echo Failed to load kernel, you may need to reflash the firmware.; " \
 	"pmenu"
 
